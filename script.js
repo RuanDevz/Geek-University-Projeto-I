97790db5d3d143a6e59221666042de0cb20b5639
RuanDevz <Ruanbatista1509@outlook.com>
2023-05-22 10:05:26 -0300

    Terminado

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const vazia = document.getElementById('vazia');
 
 cadastrar.addEventListener('click', AdicionarTarefa);
 
+// Carregar tarefas salvas do armazenamento local
+window.addEventListener('load', CarregarTarefas);
+
 function AdicionarTarefa() {
   const novaTarefa = input.value;
 
@@ -15,6 +18,9 @@ function AdicionarTarefa() {
     novoItem.addEventListener('click', ExcluirTarefa);
 
     lista.appendChild(novoItem);
+
+    // Salvar tarefas no armazenamento local
+    SalvarTarefas();
 
     input.value = '';
     vazia.style.display = 'none';
@@ -28,5 +34,35 @@ function ExcluirTarefa() {
   this.remove();
   if (lista.childElementCount === 0) {
     vazia.style.display = 'block';
+  }
+
+  // Salvar tarefas atualizadas no armazenamento local
+  SalvarTarefas();
+}
+
+function SalvarTarefas() {
+  const tarefas = Array.from(lista.children).map((item) => item.innerText);
+  localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
+function CarregarTarefas() {
+  const tarefas = localStorage.getItem('tarefas');
+  if (tarefas) {
+    const tarefasArray = JSON.parse(tarefas);
+
+    tarefasArray.forEach((tarefa) => {
+      if (tarefa.trim() !== '') {
+        const novoItem = document.createElement('li');
+        novoItem.innerText = tarefa;
+
+        novoItem.addEventListener('click', ExcluirTarefa);
+
+        lista.appendChild(novoItem);
+      }
+    });
+
+    if (lista.childElementCount > 0) {
+      vazia.style.display = 'none';
+    }
   }
 }
